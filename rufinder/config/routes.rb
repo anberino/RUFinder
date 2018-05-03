a041dc0160da8abd4c2a5bb6ad88ef91873b1c67
DarknessCatt <matheus.limacunha@hotmail.com>
2018-05-03 19:48:02 -0300

    index
    +html+assets+controller

--- rufinder/config/routes.rb
+++ rufinder/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "application#main"
+  root 'login#index'
+
+  get 'login/index'
 
   post 'users/new', to: 'users#create'
 
   resources :users
+  resources :login
 end
