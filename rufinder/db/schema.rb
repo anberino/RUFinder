e965557347e7b1e6b94ab3ef01dc4d3bd619ec99
larisala <larissa_sala@usp.br>
2018-05-04 16:15:07 -0300

    Model Reviews adicionado

--- rufinder/db/schema.rb
+++ rufinder/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504183500) do
+ActiveRecord::Schema.define(version: 20180504185157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 20180504183500) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.decimal "rating"
+    t.string "comment"
+    t.bigint "food_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_reviews_on_food_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider"
     t.string "uid"
@@ -42,4 +53,6 @@ ActiveRecord::Schema.define(version: 20180504183500) do
   end
 
   add_foreign_key "foods", "restaurants"
+  add_foreign_key "reviews", "foods"
+  add_foreign_key "reviews", "users"
 end
