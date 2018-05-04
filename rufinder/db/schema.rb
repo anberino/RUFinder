cb0be8b114f86015cc1e5b4cb77f08145f7e4bc4
Eduwzin <eduwzin@usp.br>
2018-05-04 13:08:36 -0300

    Criando o modelo Bandejão (Restaurants)

--- rufinder/db/schema.rb
+++ rufinder/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504005732) do
+ActiveRecord::Schema.define(version: 20180504160750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
