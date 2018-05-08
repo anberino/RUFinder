bc78663ffb7a67c402eceee110fb58e4070b1303
Eduwzin <eduwzin@usp.br>
2018-05-07 21:15:00 -0300

    Mudanças para o modo de produção

--- rufinder/config/routes.rb
+++ rufinder/config/routes.rb
@@ -1,28 +1,3 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'home/show'
-
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
-
-  resources :sessions, only: [:create, :destroy]
-  resource :home, only: [:show]
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: "home#show"
-
-  get 'login/index'
-
-  post 'users/new', to: 'users#create'
-
-  resources :users
-  resources :login
-
-  resources :foods
-  resources :reviews
 end
