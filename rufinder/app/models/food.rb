a9a8229a17b10491e4a3831299d0ad2d7fbd45f1
Eduwzin <eduwzin@usp.br>
2018-05-14 21:11:14 -0300

    Corrigindo os models rel. M pra N

--- rufinder/app/models/food.rb
+++ rufinder/app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
   belongs_to :restaurant
   has_many :reviews
+  has_many :users, through: :reviews
   has_many :records
 end
--- rufinder/app/models/user.rb
+++ rufinder/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :reviews
   has_many :foods, through: :reviews
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
