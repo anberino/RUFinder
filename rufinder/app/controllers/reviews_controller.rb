ab48fcf268fc34f3aefc0c8f0ad54af476f39cd3
Eduwzin <eduwzin@usp.br>
2018-05-17 20:12:25 -0300

    Action update do review controller

--- rufinder/app/controllers/reviews_controller.rb
+++ rufinder/app/controllers/reviews_controller.rb
@@ -34,6 +34,19 @@ class ReviewsController < ApplicationController
       end
     end
   end
+  
+  def update
+    @review = Review.new(review_params)
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+	  end
+	end
+  end
 
   def show
      @reviews = Review.all
