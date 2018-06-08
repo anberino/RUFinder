78fae0e61d9e461291c2091929d892e5b70ac621
Eduwzin <eduwzin@usp.br>
2018-06-07 22:48:19 -0300

    Fazendo Amigos funcionar?

--- rufinder/app/controllers/friends_controller.rb
+++ rufinder/app/controllers/friends_controller.rb
@@ -8,9 +8,9 @@ class FriendsController < ApplicationController
   end
 
   def create
-  	mail = params[:email]
-  	amigo = User.find_by(email: mail)
-  	if amigo.nil?
+  	friend = params.require(:friend).permit(:email)
+  	@amigo = User.find_by(email: friend[:email])
+  	if @amigo.nil?
   		flash[:error] = "Esta pessoa não foi encontrada... Tem certeza de que digitou o e-mail corretamente?"
   		redirect_to foods_url
   	end
@@ -18,7 +18,8 @@ class FriendsController < ApplicationController
   		flash[:error] = "Você nem existe de verdade e quer ter amigos, wtf"
   		redirect_to root_url
   	end
-  	@friend = Friend.new(sender_id: current_user.id, receiver_id: amigo.id)
+    print("Current_user id: #{current_user.id} e Amigo:#{@amigo.id}")
+  	@friend = Friend.new(sender_id: current_user.id, receiver_id: @amigo.id)
   	if @friend.save
   		flash[:notice] = "Seu pedido de amizade foi enviado"
   	else
@@ -41,5 +42,10 @@ class FriendsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def friend_params
+      params.require(:friend).permit(:email)
+    end
 
 end
