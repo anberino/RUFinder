e180042e361a89841af9295eace0ee64ccfb72fd
Eduwzin <eduwzin@usp.br>
2018-06-12 22:51:51 -0300

    Alterações visuais no menu, estrelas e comidas.

--- rufinder/app/helpers/foods_helper.rb
+++ rufinder/app/helpers/foods_helper.rb
@@ -2,23 +2,18 @@ module FoodsHelper
 
   def info(food_id)
     soma = 0
-    comments = []
-    users = []
+    review = []
     i = 0
-    print("TESTEDSADASDASDSADSDADD")
-    print("FOODID:#{food_id}")
     Review.where(food_id: food_id).each do |r|
       soma += r.rating
-      comments[i] = r.comment
-      users[i] = r.user_id
+      review[i] = r
       i = i +1
     end
-    print("A PORRA DO I TA COM :#{i}")
     if(i>0)
       media = soma/(i)
     else
       media = 5
     end
-    return comments, users, media
+    return review, media
   end
 end
--- rufinder/app/helpers/main_helper.rb
+++ rufinder/app/helpers/main_helper.rb
@@ -6,26 +6,26 @@ module MainHelper
 
 		date = Date.today
 
-		bandeja = Array.new()        
-		nota = Array.new()           
+		bandeja = Array.new()
+		nota = Array.new()
 
-		0.upto(3) do |i|             
-			bandeja[i] = Array.new()  
-			nota[i] = 0               
-		end 
+		0.upto(3) do |i|
+			bandeja[i] = Array.new()
+			nota[i] = 0
+		end
 
-		@records.where(meal: tipo).where(date: date).find_each do |r|                                    
-			food = Food.find(r.food_id)                                                                   
-			bandeja[food.restaurant_id-1].push(food.name)                                                 
-			review = Review.where(food_id: food.id).where(user_id: current_user.id).first                 
-			nota[food.restaurant_id-1] = nota[food.restaurant_id-1] + review.rating - 5 if !review.nil?   
-		end 
+		@records.where(meal: tipo).where(date: date).find_each do |r|
+			food = Food.find(r.food_id)
+			bandeja[food.restaurant_id-1].push(food)                                                 
+			review = Review.where(food_id: food.id).where(user_id: current_user.id).first
+			nota[food.restaurant_id-1] = nota[food.restaurant_id-1] + review.rating - 5 if !review.nil?
+		end
 
-		0.upto(3) do |i| 
-			if bandeja[i].size < 2 
-				nota[i] = -999     
-			end                    
-		end 
+		0.upto(3) do |i|
+			if bandeja[i].size < 2
+				nota[i] = -999
+			end
+		end
 
 		return bandeja, nota
 
