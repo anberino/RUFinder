afe8869a0217ddaff2b5e33da4886a01d1e3f3f7
Eduwzin <eduwzin@usp.br>
2018-06-14 23:47:32 -0300

    Exibição nova de comentários, correção da média,mudanças no css e mais

--- rufinder/app/helpers/application_helper.rb
+++ rufinder/app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
     def starRating(nota)
-      rate = nota/2.to_i
+      rate = (nota/2).to_i
       half = nota/2.0 - rate
       for i in 0...rate
           yield('<span class="fa fa-star checked"></span>')
