d791ed5c864850df13cade12e3530700ec19fe73
Eduwzin <eduwzin@usp.br>
2018-06-20 22:42:58 -0300

    Alteração leve no Helper Foods.

--- rufinder/app/helpers/foods_helper.rb
+++ rufinder/app/helpers/foods_helper.rb
@@ -3,9 +3,11 @@ module FoodsHelper
   def info(food_id)
     soma = 0
     review = []
+    distrib = [0,0,0,0,0,0]
     i = 0
     Review.where(food_id: food_id).each do |r|
       soma += r.rating
+      distrib[(r.rating/2).ceil] += 1
       review[i] = r
       i = i +1
     end
@@ -14,6 +16,6 @@ module FoodsHelper
     else
       media = 5
     end
-    return review, media
+    return review, media, distrib
   end
 end
