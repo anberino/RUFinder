fd2714156a0a34ea0165429a74da3d059a09c023
larisala <larissa_sala@usp.br>
2018-06-22 19:04:46 -0300

    Ajustes

--- rufinder/db/seeds.rb
+++ rufinder/db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 Restaurant.create(name:"Central", location:"Praça do Relógio Solar, Travessa 8, 300")
 Restaurant.create(name:"Física" , location:"Rua do Matão, 1371")
 Restaurant.create(name:"Química", location:"Av. Prof. Lineu Prestes, 748")
