3da427f060750cf4dd02a14ec98e3f57cafe1f35
larisala <larissa_sala@usp.br>
2018-06-27 20:34:18 -0300

    Notificação de pedidos de amizade

--- rufinder/app/controllers/main_controller.rb
+++ rufinder/app/controllers/main_controller.rb
@@ -2,6 +2,7 @@ class MainController < ApplicationController
   def index
     if(current_user)
       @records = Record.all
+      @friends = Friend.all
     else
       redirect_to root_path
     end
